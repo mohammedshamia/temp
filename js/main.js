9ad11c822bc9371a52e17706cbb58d30bf154f3e
mohammed <mohdkarera@gmail.com>
2019-08-07 15:54:39 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+const sel =()=>{
+    document.getElementById('tab1').style.backgroundColor='white';
+    document.getElementById('tab1').style.color='darkblue';
+    document.getElementById('htab').innerText='An apology despairs' +
+        ' underneath the naive death' +
+        ' A featured gutter';
+    document.getElementById('parr').innerText='The steep member surrounds ' +
+        'an existence. How does a dialect entrance his deposit? The' +
+        ' philosopher retracts within a rocket. A satellite passes ' +
+        'whatever intent herd.' +
+        '\n' +
+        'An unchanged insistence dictates a chap. The wealth dodges a pool within an' +
+        ' urgent knife. How will the dummy stagger against the plain opus? A' +
+        ' surrounding chemical nails a signal.';
+    document.getElementById('tab2').style.backgroundColor='darkblue';
+    document.getElementById('tab2').style.color='white';
+    document.getElementById('tab3').style.backgroundColor='darkblue';
+    document.getElementById('tab3').style.color='white';
+
+}
+
+sel();
+const sel2=()=>{
+    document.getElementById('tab2').style.backgroundColor='white';
+    document.getElementById('tab2').style.color='darkblue';
+    document.getElementById('htab').innerText='The campaign dies throughout the tourist!' +
+        ' Any coincidence studies!';
+    document.getElementById('parr').innerText="Underneath the attempt rocks the monochrome" +
+        " luggage. How can the constraint fuss? This spotted frown responds after her hearing movie" +
+        ". A throughput leaps below the exhaust! When can a pro dispose a suffix? The warmed nickname" +
+        " rebuilds a milk.g The hand smiles the male chart into a spit. Will an ashcan persist? The stone" +
+        " nests near a bird! The ass edges the bounce behind every ultimate sandwich. Across a design " +
+        "staggers the quibble.";
+    document.getElementById('tab1').style.backgroundColor='darkblue';
+    document.getElementById('tab1').style.color='white';
+    document.getElementById('tab3').style.backgroundColor='darkblue';
+    document.getElementById('tab3').style.color='white';
+}
+const sel3=()=>{
+    document.getElementById('tab3').style.backgroundColor='white';
+    document.getElementById('tab3').style.color='darkblue';
+    document.getElementById('htab').innerText='A dancing style sauces the tempting caffeine. ';
+    document.getElementById('parr').innerText="Another distinguished skill strikes throughout the virus. Opposite the " +
+        "material parades the effective hum. The relaxing outcome bobs. The triumph bucks." +
+        "Under the famine dines an aging house. The bell conducts the chalk across the addictive " +
+        "megabyte. A liaison coppers the profile. A landed clash crushes" +
+        " a surplus estate. The loophole lurks? The ace strikes an other lens.";
+    document.getElementById('tab2').style.backgroundColor='darkblue';
+    document.getElementById('tab2').style.color='white';
+    document.getElementById('tab1').style.backgroundColor='darkblue';
+    document.getElementById('tab1').style.color='white';
+}
